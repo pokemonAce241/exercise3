2bea38a3a07a01082990b6e43cee5af1c0e87b89
bwatson <bwatson@ncsu.edu>
2017-06-13 23:44:34 -0400

    debug

--- drawstuff.js
+++ drawstuff.js
@@ -390,7 +390,7 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Define a rectangle in 2D with colors and coords at corners
-    var globals = { light: new Vector(50,50,100); // light over left upper rect
+    var globals = { light: new Vector(50,50,100)}; // light over left upper rect
     var tlAttribs = { diffuse: new Color(255,0,0)};
     var trAttribs = { diffuse: new Color(0,255,0)};
     var brAttribs = { diffuse: new Color(0,0,255)};
