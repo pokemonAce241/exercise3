c375eacf729f1e9c2c45f7f8679cfd29228fd27c
bwatson <bwatson@ncsu.edu>
2017-06-13 23:48:32 -0400

    debug

--- drawstuff.js
+++ drawstuff.js
@@ -395,6 +395,6 @@ function main() {
     var trAttribs = { diffuse: new Color(0,255,0)};
     var brAttribs = { diffuse: new Color(0,0,255)};
     var blAttribs = { diffuse: new Color(0,0,0)};
-    interpRect(imagedata,50,150,50,200,tlAttribs,trAttribs,brAttribs,blAttribs);
+    interpRect(imagedata,50,150,50,200,globals,tlAttribs,trAttribs,brAttribs,blAttribs);
     context.putImageData(imagedata,0,0); // display the image in the context
 } // end main
