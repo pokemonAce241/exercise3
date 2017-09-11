4d747e00c38adc65349b347e9f611cc2992071ab
jdbencke <jdbencke@ncsu.edu>
2017-09-10 22:32:13 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -406,13 +406,17 @@ function main() {
     var globals = { lightPos: new Vector(150,150,15),  // light over left upper rect
                     lightCol: new Color(255,255,255)}; // light is white
     var tlAttribs = { diffuse: new Color(0,0,255),
-                      ambient: new Color(0,255,0)};    // all four rect verts blue
+                      ambient: new Color(0,255,0),
+                      specular: new Color(255,255,255)};    // all four rect verts blue
     var trAttribs = { diffuse: new Color(0,0,255),
-                      ambient: new Color(0,255,0)};
+                      ambient: new Color(0,255,0),
+                      specular: new Color(255,255,255)};
     var brAttribs = { diffuse: new Color(0,0,255),
-                      ambient: new Color(0,255,0)};
+                      ambient: new Color(0,255,0),
+                      specular: new Color(255,255,255)};
     var blAttribs = { diffuse: new Color(0,0,255),
-                      ambient: new Color(0,255,0)};
+                      ambient: new Color(0,255,0),
+                      specular: new Color(255,255,255)};
     interpRect(imagedata,50,150,50,200,globals,tlAttribs,trAttribs,brAttribs,blAttribs);
     context.putImageData(imagedata,0,0); // display the image in the context
 } // end main
